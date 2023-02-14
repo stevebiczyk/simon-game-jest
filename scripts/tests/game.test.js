f5698c8bec8282c7f8fbd5ba9bf0f5012bbb783e
stevebiczyk <stevebiczyk@gmail.com>
2023-02-14 22:53:07 +0000

    Added showTurn function and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -7,7 +7,8 @@ const {
     newGame,
     showScore,
     addTurn,
-    lightsOn
+    lightsOn,
+    showTurns
 } = require("../game");
 
 beforeAll(() => {
@@ -33,6 +34,9 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
     });
 });
 
@@ -78,5 +82,10 @@ describe("gameplay works correctly", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain(game.currentGame[0] + "light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
